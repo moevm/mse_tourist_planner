c3b6c768a446bc92b92cb4381c0868cc720c502d
Yura19988991 <yura19988991@mail.ru>
2018-12-03 19:56:27 +0300

    Delete action.js

--- tourist_planner/myapps/static/main/bootstrap/action.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    $.getJSON('auction/all',{}, function (j) {
-        console.log(j);
-        $("#newDate").val(j.date);
-        $("#newTime").val(j.time);
-        $("#newTimeout").val(j.timeout);
-        $("#newInterval").val(j.interval);
-    })
-};
